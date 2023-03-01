bad49e42ba30bb64646b1846ad6d3788ebe7760b
nick <nifuller@protonmail.com>
2023-03-01 14:52:50 -0800

    fixed input error and divided temps by 10

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import plotly.express as px
 from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
-place = st.text_input("Place: ")
-days = st.slider("Forecast Days", min_value=1, max_value=5,
-                 help="Select the number of forecasted days")
-option = st.selectbox("select data to view", 
-                      ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
+try:
+    place = st.text_input("Place: ")
+    days = st.slider("Forecast Days", min_value=1, max_value=5,
+                    help="Select the number of forecasted days")
+    option = st.selectbox("select data to view", 
+                        ("Temperature", "Sky"))
+    st.subheader(f"{option} for the next {days} days in {place}")
 
-if place:
-    filtered_data = get_data(place, days)
+    if place:
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] /10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png",
-                  "Rain":"images/rain.png", "Snow":"images/snow.png"}
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_path = [images[condition] for condition in sky_conditions]
-        st.image(image_path, width=115)
+        if option == "Sky":
+            images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png",
+                    "Rain":"images/rain.png", "Snow":"images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_conditions]
+            st.image(image_path, width=115)
+except KeyError:
+    st.warning("Invalid place")
